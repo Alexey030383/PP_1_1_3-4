084bed5edba243bfd95ec7fe3911be613fe89599
Alexey030383 <alexey_baryshev@mail.ru>
2023-11-01 01:41:50 +0300

    PP 1.1.3.4 is done

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -6,5 +6,14 @@ public class Main {
     public static void main(String[] args) {
         UserServiceImpl userService = new UserServiceImpl();
         userService.createUsersTable();
+        userService.saveUser("Alexey", "Baryshev", (byte) 40);
+        userService.saveUser("Vitaly", "Baryshev", (byte) 11);
+        userService.saveUser("Maxim", "Baryshev", (byte) 2);
+        userService.saveUser("Anastasya", "Baryshev", (byte) 37);
+        userService.removeUserById(1);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+        userService.closeConnectionDB();
     }
 }
